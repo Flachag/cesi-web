084c4fd9e8249a942ce675e8bb24079b533fdd78
Flavien Chagras <flavien.chagras@apitech-solution.com>
2021-02-08 22:37:44 +0100

    :sparkles: register back end

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\User;
+use Exception;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class UsersController extends Controller{
+    public function register(Request $request, Response $response, array $args): Response {
+        $this->render($response, 'templates/pages/register.twig');
+    }
+
+    public function postRegister(Request $request, Response $response, array $args): Response {
+        try {
+            $username = filter_var($request->getParsedBodyParam('username'), FILTER_SANITIZE_STRING);
+            $password = filter_var($request->getParsedBodyParam('password'), FILTER_SANITIZE_STRING);
+            $password_conf = filter_var($request->getParsedBodyParam('password_conf'), FILTER_SANITIZE_STRING);
+            $email = filter_var($request->getParsedBodyParam('email'), FILTER_SANITIZE_STRING);
+            $firstname = filter_var($request->getParsedBodyParam('firstname'), FILTER_SANITIZE_STRING);
+            $lastname = filter_var($request->getParsedBodyParam('lastname'), FILTER_SANITIZE_STRING);
+            $address = filter_var($request->getParsedBodyParam('address'), FILTER_SANITIZE_STRING);
+
+            if (mb_strlen($username, 'utf8') < 3 || mb_strlen($username, 'utf8') > 35) throw new Exception("Votre pseudo doit contenir entre 3 et 35 caractères.");
+            if (mb_strlen($lastname, 'utf8') < 1 || mb_strlen($lastname, 'utf8') > 50) throw new Exception("Votre nom doit contenir entre 2 et 50 caractères.");
+            if (mb_strlen($firstname, 'utf8') < 1 || mb_strlen($firstname, 'utf8') > 50) throw new Exception("Votre prénom doit contenir entre 2 et 50 caractères.");
+            if (mb_strlen($password, 'utf8') < 8) throw new Exception("Votre mot de passe doit contenir au moins 8 caractères.");
+            if (User::where('username', '=', $username)->exists()) throw new Exception("Ce pseudo est déjà pris.");
+            if (User::where('email', '=', $email)->exists()) throw new Exception("Cet email est déjà utilisée.");
+            if ($password != $password_conf) throw new Exception("La confirmation du mot de passe n'est pas bonne.");
+
+            $user = new User();
+            $user->lastname = $lastname;
+            $user->firstname = $firstname;
+            $user->username = $username;
+            $user->email = $email;
+            $user->address = $address;
+            $user->password = password_hash($password, PASSWORD_DEFAULT);
+            $user->save();
+
+            $this->flash->addMessage('success', "L'utilisateur a été ajouté");
+            $response = $response->withRedirect($this->router->pathFor('login'));
+        } catch (\Exception $e){
+            $this->flash->addMessage('error', $e->getMessage());
+            $response = $response->withRedirect($this->router->pathFor("register"));
+        }
+        return $response;
+    }
+}
