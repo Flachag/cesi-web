da632e91458121ce970230f8bcabba39fb2d151e
Jules Sayer <jules.sayer@protonmail.com>
2021-02-08 22:31:03 +0100

    Added User model + Auth Helper

---
+++ app/helpers/Auth.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\helpers;
+
+use app\models\User;
+use Exception;
+
+/**
+ * Class Auth
+ * @package app\helpers
+ */
+class Auth {
+
+    public static function attempt($username, $password) {
+        try {
+            $user = User::where('username', '=', $username)->orWhere('email', '=', $username)->firstOrFail();
+            if (!password_verify($password, $user->password)) throw new Exception();
+            $_SESSION['user'] = $user;
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public static function user() {
+        $res = null;
+        if (self::check()) {
+            $_SESSION['user']->refresh();
+            $res = $_SESSION['user'];
+        }
+        return $res;
+    }
+
+    public static function check() {
+        return isset($_SESSION['user']);
+    }
+
+    public static function logout() {
+        unset($_SESSION['user']);
+    }
+}
---
+++ app/models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class User
+ * @package app\models
+ */
+class User extends Model {
+    public $timestamps = false;
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        'username',
+        'password',
+        'email',
+        'firstname',
+        'lastname',
+        'address',
+        'level'
+    ];
+}
